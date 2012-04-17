345f2be26cc096ffb12a12690e1de19a3f07d0a1
rkarukan <rkarukan@yahoo.co.jp>
2012-04-17 23:24:03 +0900

    kadai

---
+++ src/hello/HelloServlet.java
@@ -0,0 +1,13 @@
+﻿package hello;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class HelloServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
